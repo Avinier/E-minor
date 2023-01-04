8915ae5e279129abdd347fa593fd20bf947f2838
Avinier Nakamoto <adisubu.2410@gmail.com>
2023-01-04 13:26:01 +0530

    launch 1

--- src/components/ExplorePage/Explore.tsx
+++ src/components/ExplorePage/Explore.tsx
@@ -3,16 +3,18 @@ import NftCard from "./NftCard";
 
 const Explore : FC = () => {
     return (
-        <section className="bg-main w-[100vw]">
+        <section className="bg-main">
             <article className="pt-[25px] px-[150px]">
                 <h1 className="text-[60px] font-sans font-black text-accent">Explore NFTS!</h1>
                 <h3 className="text-accent font-sans text-[20px] pb-[25px]">Look at the NFTS of the most iconic lyrics in recent times.</h3>
             </article>
-            <div className="flex justify-around">
+            <div className="grid grid-cols-3 gap-[20px]">
                 <NftCard image="/nochurchinthewild.jpg"/>
                 <NftCard image="/numbencore.jpeg"/>
                 <NftCard image="/praisethelord.jpeg"/>
                 <NftCard image="/stan.jpeg"/>
+                <NftCard image="/crazyinlove.jpeg"/>
+                <NftCard image="/blindinglights.jpeg"/>
 
             </div>
         </section>
--- src/components/ExplorePage/NftCard.tsx
+++ src/components/ExplorePage/NftCard.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const NftCard = (props : any) => {
     return (
         <div>
-            <Image src={props.image} alt="iengb" className="object-cover rounded-lg" width={350} height={350}/>
+            <Image src={props.image} alt="iengb" className="object-cover rounded-lg" width={450} height={450}/>
         </div>
     )
 }
